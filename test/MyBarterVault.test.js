57d8d223baa41a1151ef359006792bfc641877d0
piweb <3.14piweb@gmail.com>
2026-02-23 09:35:11 -0500

    Update test

--- test/MyBarterVault.test.js
+++ test/MyBarterVault.test.js
@@ -10,20 +10,20 @@ describe("MyBarter "Robot Lawyer" Vault", function () {
     vault = await MyBarterVault.deploy();
   });
 
-  it("Should apply a flat $2 fee if a whitelisted NFT is present", async function () {
+  it("Should apply a flat $2.50 fee if a whitelisted NFT is present", async function () {
     const nftAddress = "0x1234567890123456789012345678901234567890";
     await vault.setCollectionStatus(nftAddress, true);
 
     const fee = await vault.calculatePlatformFee([nftAddress], 0);
-    // 2 * 10^18 (Flat fee in 18 decimals)
-    expect(fee).to.equal(ethers.parseEther("2"));
+    // 2.50 * 10^18 (Flat fee in 18 decimals)
+    expect(fee).to.equal(ethers.parseEther("2.50"));
   });
-
-  it("Should apply a 0.5% commission if no whitelisted NFT is present", async function () {
+  
+  it("Should apply a 0.75% commission if no whitelisted NFT is present", async function () {
     const tokenValueUSD = ethers.parseEther("1000"); // $1000 value
     const fee = await vault.calculatePlatformFee([], tokenValueUSD);
     
-    // 0.5% of $1000 = $5
+    // 0.75% of $1000 = $7.50
     expect(fee).to.equal(ethers.parseEther("5"));
   });
 
@@ -32,7 +32,7 @@ describe("MyBarter "Robot Lawyer" Vault", function () {
     const tokenValueUSD = ethers.parseEther("1000");
 
     const fee = await vault.calculatePlatformFee([dustNFT], tokenValueUSD);
-    // Should still be 0.5% ($5) because the NFT isn't whitelisted
-    expect(fee).to.equal(ethers.parseEther("5"));
+    // Should still be 0.75% ($7.50) because the NFT isn't whitelisted
+    expect(fee).to.equal(ethers.parseEther("7.50"));
   });
 });
